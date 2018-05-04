b129995a431bcd613a771ee70e2fc88fdfd69ba2
drissiReda <mohamedreda.drissi@edu.uca.ma>
2018-05-04 23:06:00 +0200

    Added some questionable comments

--- FA.cpp
+++ FA.cpp
@@ -27,7 +27,7 @@ void FA<T>::add_transition(int src, T input, int dest)
 {
 	m_transitions.insert(pair<pair<int,T>, int>(pair<int,T>(src, input), dest));
 }
-
+//useful if you wanna loop
 template<typename T>
 void FA<T>::reset()
 {
@@ -38,7 +38,7 @@ template<typename T>
 int FA<T>::input(T inp)
 {
 	auto tr = make_pair(m_state, inp);
-  //find if the transition already exists 
+  //find if the transition exists or not
 	if(m_transitions.count(tr)>0)
 	{
 		auto it = m_transitions.find(tr);
@@ -46,16 +46,21 @@ int FA<T>::input(T inp)
 	}
 	else
 	{
+    //this transition is a fraud so we return -1
+    //for the sake of violence we could raise a SIGTERM
+    //or throw an exception, make them panic.
 		return m_state = -1;
 	}
 }
 
+//checking the boolean value of acceptance
 template<typename T>
 bool FA<T>::is_accepting()
 {
 	return m_final_states.count(m_state)!=0;
 }
 
+//this requires no comments whatsoever
 template<typename T>
 int FA<T>::state()
 {
