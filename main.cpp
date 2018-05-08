c17c9ea7492057ebacbc2c8aa26e66fb8b0c7838
drissiReda <mohamedreda.drissi@edu.uca.ma>
2018-05-09 00:58:28 +0200

    Fixed display eof

--- main.cpp
+++ main.cpp
@@ -26,6 +26,8 @@ FA constructFA()
 	while(true)
 	{
 		cin >> st;
+		if(cin.eof())
+		 	break;
 		cout << "Is it accepting? [y/N]" << endl;
 		cin >> inp;
 		if(inp=='y' || inp=='Y')
@@ -37,9 +39,9 @@ FA constructFA()
 			cout << "Did not quite get that, but I'll take it as a no" << endl;
 		}
 		fa.add_state(inp,acc);
-		if(cin.eof()) break;
 		cout << "Next state :[You can exit with Ctrl+D]"<<endl;
 	}
+	cin.clear();
 	//Transitions
 	cout << "Enter your transitions" << endl;
 	int dst;
@@ -47,14 +49,16 @@ FA constructFA()
 	{
 		cout << "Source of the transition " << endl;
 		cin >> st;
+		if(cin.eof())
+		 	break;
 		cout << "Element to consume " << endl;
 		cin >> inp;
 		cout << "Destination of the transition" << endl;
 		cin >> dst;
 		fa.add_transition(st,inp,dst);
-		if(cin.eof()) break;
 		cout << "Next transition :[You can exit with Ctrl+D]"<<endl;
 	}
+	cin.clear();
 	return fa;
 }
 void simulate()
@@ -70,6 +74,7 @@ void simulate()
 		if(cin.eof()) break;
 		fa.input(inp);
 	}
+	cin.clear();
 }
 void concatenate()
 {
@@ -104,6 +109,17 @@ void NFA2DFA()
 	int i=0;
 	fa.determinize(&i);
 }
+void disp_menu()
+{
+	cout << "Choose an option " << endl;
+	cout << "--------------------" << endl;
+	cout << "1.for simulating"    <<endl;
+	cout << "2.for a Kleene Star" <<endl;
+	cout << "3.for a Kleene |"    <<endl;
+	cout << "4.for concatenation" <<endl;
+	cout << "5.for converting to DFA" << endl;
+	cout << "Enter any other key to quit" << endl;
+}
 int main(int argc, char* argv[])
 {
 	FA fa(0, false);
@@ -115,18 +131,15 @@ int main(int argc, char* argv[])
 	fa.add_transition(1,'c',3);
 	fa.add_transition(3,'a',0);
 	fa.add_transition(3,'b',0);
-
-	cout << "Choose an option " << endl;
-	cout << "--------------------" << endl;
-	cout << "1.for simulating"    <<endl;
-	cout << "2.for a Kleene Star" <<endl;
-	cout << "3.for a Kleene |"    <<endl;
-	cout << "4.for concatenation" <<endl;
-	cout << "5.for converting to DFA" << endl;
-	cout << "Enter any other key to quit" << endl;
-	int in;
+	//fa.conTo(fa);
+	disp_menu();
+	int in=0;
 	while(true)
 	{
+		if(in>0 && in<6)
+		{
+			disp_menu();
+		}
 		cin>>in;
 		switch(in)
 		{
@@ -137,6 +150,8 @@ int main(int argc, char* argv[])
 			case 5 : NFA2DFA(); break;
 			default: return 0;
 		}
+		if(cin.eof())
+			break;
 	}
 	return 0;
 }
